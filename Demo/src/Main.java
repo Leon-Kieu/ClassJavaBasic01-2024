1bbaf03d491aff67e8e57bc687b62cae4238c957
Leon Kieu <143923617+Leon-Kieu@users.noreply.github.com>
2024-04-23 19:47:24 +0700

    Update Main.java

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Xin chao lop Java Basic 01-2024");
         System.out.println("Rất vui vì được đồng hành cùng các bạn ở lớp Java Basic");
+        System.out.println("Test trường hợp Pull code về");
     }
 }
