1cdf912771a0dd142a8eab03c071f21013ddefc6
Leon <leon.kieu@gmail.com>
2024-04-23 19:53:39 +0700

    update out-put buổi 1

--- Demo/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        System.out.println("Xin chao lop Java Basic 01-2024");
-        System.out.println("Rất vui vì được đồng hành cùng các bạn ở lớp Java Basic");
-        System.out.println("Test trường hợp Pull code về");
-    }
-}
---
+++ Demo_Bai_Giang/src/Buoi1/Buoi1.java
@@ -0,0 +1,10 @@
+package Buoi1;
+
+public class Buoi1 {
+    public static void main (String[] agrs){
+        System.out.println("Out put buổi 1");
+        System.out.println("Done cài đặc JDK");
+        System.out.println("Done cài đặc IJ");
+        System.out.println("Tạo dc project git và gửi link cho anh");
+    }
+}
